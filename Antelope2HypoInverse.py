302593d62d00b89ba8838e38e3a60ccfef50f1b1
rtarzan <rachel.marzen@gmail.com>
2019-10-23 14:57:08 -0400

    finished preliminary loop through picks to output, terminator line

--- Antelope2HypoInverse.py
+++ Antelope2HypoInverse.py
@@ -75,7 +75,7 @@ def writeLength(oldstr, newstr):
     if len(str(oldstr)) == len(str(newstr)):
         accstr=str(newstr)
     else:
-        print('Cannot change string, check proposed string length')
+        print('Cannot change string, '+newstr+' check proposed string length')
     return accstr
     
 def writeMdhm(etime):
@@ -228,116 +228,164 @@ def write2Hypoinverse(eventdb, ffname):
     
     #PART 2: PICK LINES
     #loop through picks
-    
-    
-    #initialize pick data lines
-    statcode = wsp*5 #left justified 5 letter station code (A5)
-    statnet = wsp*2 #seismic network code (A2)
-    comp1code = wsp*1 #station component code 1 letter (A1)
-    comp3code = wsp*3 #station component code 3 letter (A3)
-    prmk = wsp*2 #P remark (A2)
-    pfm = wsp*1 #P first motion (A1)
-    pweightcode = wsp*1 #P weight code (I1)
-    pyr = wsp*4 #P pick year (I4)
-    pmdhm = wsp*8 #P pick month, day, hour, minute (4I2)
-    psec = wsp*5 #P pick second (F5.2)
-    presid = wsp*4 #P pick residual (F4.2)
-    normpwt = wsp*3 #normalized P weight (F3.2)
-    ssec = wsp*5 #S arrival second (F5.2)
-    srmk = wsp*2 #S remark (A2)
-    sweightcode = wsp*1 #s weight code (I1)
-    sresid = wsp*4 #s travel time residual (F4.2)
-    amp = wsp*7 #amplitude peak-to-peak (F7.2)
-    ampunitcd = wsp*2 #amp units code (I2)
-    normswt = wsp*3 #s weight used (F3.2)
-    pdelay = wsp*4 #P delay time (F4.2)
-    sdelay = wsp*4 #S delay time (F4.2)
-    epidist = wsp*4 #epicentral distance (F4.1)
-    emang = wsp*3 #emergence angle at source (F3.0)
-    ampmagwtcode = wsp*1 #amplitude magnitude weight code (I1)
-    durmagwtcode = wsp*1 #duration magnitude weight code (I1)
-    amppd = wsp*3 #period at which station amplitude measured (F3.2)
-    statrmk = wsp*1 #station remark (A1)
-    codadur = wsp*4 #coda duration in s (F4.0)
-    azi2stat = wsp*3 #azimuth to station in deg E of N (F3.0)
-    durmag = wsp*3 #station duration magnitude (F3.2)
-    ampmag = wsp*3 #amplitude magnitude (F3.2)
-    impP = wsp*4 #importance of P arrival (F4.3)
-    impS = wsp*4 #importance of S arrival (F4.3)
-    dscode = wsp*1 #data source code (A1)
-    durmagcode = wsp*1 #duration magnitude code (A1)
-    ampmagcode = wsp*1 #amplitude magnitude code (A1)
-    statloccode = wsp*2 #2 letter stat location code (A2)
-    amptype = wsp*2 #amplitude type (I2)
-    acomp3code = wsp*3 #alternate 3-letter component code (A3)
-    ampmagyn = wsp*1 #X if station amplitude mag not used in event mag (A1)
-    durmagyn = wsp*1 #X if station duration mag not used in event mag (A1)
-    
-    #set necessary variables to input database values
-    
-    statcode = writeLength(statcode,"{:<5}".format(eventdb['sta'].iloc[0]))
-    
-    statnet = writeLength(statnet,"{:<2}".format(eventdb['netwk'].iloc[0]))
-
-    if 'Z' in eventdb['chan'].iloc[0]:
-        comp1code = writeLength(comp1code,'V')
-    else:
-        comp1code = writeLength(comp1code,'H')
+    for iind, erow in eventdb.iterrows():
         
-    comp3code = writeLength(comp3code,"{:<3}".format(eventdb['chan'].iloc[0]))
-    
-    if eventdb['iphase'].iloc[0] == 'P':
-        prmk = writeLength(prmk,'iP')
-    
-        if eventdb['fm'].iloc[0] == 'U':
-            pfm = writeLength(pfm,'U')
-        elif eventdb['fm'].iloc[0] == 'D':
-            pfm = writeLength(pfm,'D')
-
-        #P weight code - LATER MAKE A SMARTER WEIGHTING SCHEME
-        #MAYBE DOWNWEIGHT ARRIVALS FURTHER AWAY
-        pweightcode = writeLength(pweightcode,2)
-    
-        #year
-        pyr = writeLength(pyr,time.gmtime(eventdb['time_arriv'].iloc[0]).tm_year)    
-    
-        #mdhm
-        pmdhm = writeLength(pmdhm,writeMdhm(eventdb['time_arriv'].iloc[0])) 
-    
-        #second
-        psec = writeLength(psec,int(time.gmtime((eventdb['time_arriv'].\
-        iloc[0])).tm_sec*100+round(eventdb['time_arriv'].iloc[0]%1*100,0)))
-    elif eventdb['iphase'].iloc[0] == 'S':
-        srmk = writeLength(srmk,'ES')
+        #initialize pick data lines
+        statcode = wsp*5 #left justified 5 letter station code (A5)
+        statnet = wsp*2 #seismic network code (A2)
+        comp1code = wsp*1 #station component code 1 letter (A1)
+        comp3code = wsp*3 #station component code 3 letter (A3)
+        prmk = wsp*2 #P remark (A2)
+        pfm = wsp*1 #P first motion (A1)
+        pweightcode = wsp*1 #P weight code (I1)
+        pyr = wsp*4 #P pick year (I4)
+        pmdhm = wsp*8 #P pick month, day, hour, minute (4I2)
+        psec = wsp*5 #P pick second (F5.2)
+        presid = wsp*4 #P pick residual (F4.2)
+        normpwt = wsp*3 #normalized P weight (F3.2)
+        ssec = wsp*5 #S arrival second (F5.2)
+        srmk = wsp*2 #S remark (A2)
+        sweightcode = wsp*1 #s weight code (I1)
+        sresid = wsp*4 #s travel time residual (F4.2)
+        amp = wsp*7 #amplitude peak-to-peak (F7.2)
+        ampunitcd = wsp*2 #amp units code (I2)
+        normswt = wsp*3 #s weight used (F3.2)
+        pdelay = wsp*4 #P delay time (F4.2)
+        sdelay = wsp*4 #S delay time (F4.2)
+        epidist = wsp*4 #epicentral distance (F4.1)
+        emang = wsp*3 #emergence angle at source (F3.0)
+        ampmagwtcode = wsp*1 #amplitude magnitude weight code (I1)
+        durmagwtcode = wsp*1 #duration magnitude weight code (I1)
+        amppd = wsp*3 #period at which station amplitude measured (F3.2)
+        statrmk = wsp*1 #station remark (A1)
+        codadur = wsp*4 #coda duration in s (F4.0)
+        azi2stat = wsp*3 #azimuth to station in deg E of N (F3.0)
+        durmag = wsp*3 #station duration magnitude (F3.2)
+        ampmag = wsp*3 #amplitude magnitude (F3.2)
+        impP = wsp*4 #importance of P arrival (F4.3)
+        impS = wsp*4 #importance of S arrival (F4.3)
+        dscode = wsp*1 #data source code (A1)
+        durmagcode = wsp*1 #duration magnitude code (A1)
+        ampmagcode = wsp*1 #amplitude magnitude code (A1)
+        statloccode = wsp*2 #2 letter stat location code (A2)
+        amptype = wsp*2 #amplitude type (I2)
+        acomp3code = wsp*3 #alternate 3-letter component code (A3)
+        ampmagyn = wsp*1 #X if station amplitude mag not used in event mag (A1)
+        durmagyn = wsp*1 #X if station duration mag not used in event mag (A1)
         
-        sweightcode = writeLength(sweightcode,2)
+        #set necessary variables to input database values
         
+        statcode = writeLength(statcode,"{:<5}".format(erow['sta']))
         
+        statnet = writeLength(statnet,"{:<2}".format(erow['netwk']))
     
-    #amplitude
-    #amplitude units
-    #duration magnitude weight (4)    
-    #coda duration in s (0)
-    #station duration mag (0)
-    
-    #concat to single line
-    pline = statcode+statnet+wsp+comp1code+comp3code+wsp+prmk+pfm+\
-    pweightcode+pyr+pmdhm+psec+presid+normpwt+ssec+srmk+wsp+\
-    sweightcode+sresid+amp+ampunitcd+normswt+pdelay+sdelay+epidist+\
-    emang+ampmagwtcode+durmagwtcode+amppd+statrmk+codadur+azi2stat+\
-    durmag+ampmag+impP+impS+dscode+durmagcode+ampmagcode+statloccode+\
-    amptype+acomp3code+ampmagyn+durmagyn+'\n' 
-    
-    #write to event file
-    with open(ffname, 'a') as the_file:
-        if len(pline) != 121: #check that pick line is the correct length 
-            print('error: pick line is incorrect length, not writing pick'+\
-            'to file')        
+        if 'Z' in erow['chan']:
+            comp1code = writeLength(comp1code,'V')
         else:
-            the_file.write(pline)
+            comp1code = writeLength(comp1code,'H')
             
+        comp3code = writeLength(comp3code,"{:<3}".format(erow['chan'][:3]))
+        
+        #write P versus S arrival info in different variables    
+        if erow['iphase'] == 'P':
+            prmk = writeLength(prmk,'iP')
+        
+            #assign first motion direction if available            
+            if erow['fm'] == 'U':
+                pfm = writeLength(pfm,'U')
+            elif erow['fm'] == 'D':
+                pfm = writeLength(pfm,'D')
+    
+            #LATER MAKE A SMARTER WEIGHTING SCHEME DOWNWEIGHT ARRIVALS FURTHER AWAY
+            pweightcode = writeLength(pweightcode,2)
+    
+            pyr = writeLength(pyr,time.gmtime(erow['time_arriv']).tm_year)    
+    
+            pmdhm = writeLength(pmdhm,writeMdhm(erow['time_arriv'])) 
+    
+            psec = writeLength(psec,"{:>5}".format(int(time.gmtime\
+            ((erow['time_arriv'])).tm_sec*100+round(erow['time_arriv']\
+            %1*100,0))))
+            
+        elif erow['iphase'] == 'S':
+            srmk = writeLength(srmk,'ES')
+            
+            sweightcode = writeLength(sweightcode,2)
+            
+            ssec = writeLength(ssec,"{:>5}".format(int(time.gmtime\
+            ((erow['time_arriv'])).tm_sec*100+\
+            round(erow['time_arriv']%1*100,0))))
+    
+        #placeholder 0s or no weights(4) where no measurements available
+        amp = writeLength(amp, "{:>7}".format(0))    
+    
+        ampunitcd = writeLength(ampunitcd, "{:>2}".format(0))
+    
+        durmagwtcode = writeLength(durmagwtcode, 4)
+    
+        codadur = writeLength(codadur, "{:>4}".format(0))    
+    
+        durmag = writeLength(durmag, "{:>3}".format(0))
+        
+        #concat to single line
+        pline = statcode+statnet+wsp+comp1code+comp3code+wsp+prmk+pfm+\
+        pweightcode+pyr+pmdhm+psec+presid+normpwt+ssec+srmk+wsp+\
+        sweightcode+sresid+amp+ampunitcd+normswt+pdelay+sdelay+epidist+\
+        emang+ampmagwtcode+durmagwtcode+amppd+statrmk+codadur+azi2stat+\
+        durmag+ampmag+impP+impS+dscode+durmagcode+ampmagcode+statloccode+\
+        amptype+acomp3code+ampmagyn+durmagyn+'\n' 
+        
+        #write to event file
+        with open(ffname, 'a') as the_file:
+            if len(pline) != 121: #check that pick line is the correct length 
+                print('error: pick line is incorrect length, not writing pick'+\
+                'to file')        
+            else:
+                the_file.write(pline)
+                
+        #END OF LOOP FOR WRITING PICK LINES
+                
             
     #PART 3: TERMINATOR LINE
+    #initialize terminator line variables to correct length        
+    trialhrmin = wsp*4 #trial hour and minute (A4)
+    trialsec = wsp*4 #trial second (2I2)
+    triallatdeg = wsp*2 #trial latitude (F2.0)
+    triallatmin = wsp*4 #trial latitude minutes (F4.2)
+    triallongdeg = wsp*3 #trial longitude (F3.0)
+    triallongmin= wsp*4 #trial longitude minutes (F4.2)
+    trialdepth = wsp*5 #trial depth (F5.2)
+    trialidnum = wsp*10 #trial ID number (I10)
+    
+    #set variables to proper input parameters - just event ID for now
+    #this is being skipped because current documentation doesn't clearly 
+    #indicate if negative degree locations are appropriately considered by
+    #hypoinverse code when in terminator line; inputting preliminary origin
+    #in header line instead
+    
+    #trialhrmin = writeLength(trialhrmin,mdhm[4:])
+    #trialsec = writeLength(trialsec,ortime)
+    #triallatdeg = writeLength(triallatdeg,latdeg)
+    #triallatmin = writeLength(triallatmin,latmin)
+    #triallongdeg = writeLength(triallongdeg,londeg)
+    #triallongmin = writeLength(triallongmin,lonmin)
+    #trialdepth = writeLength(trialdepth,dep)
+    trialidnum = writeLength(trialidnum,evid)
+    
+
+    #concat to a single line
+    termline=wsp*6+trialhrmin+trialsec+triallatdeg+wsp+triallatmin+\
+    triallongdeg+wsp+triallongmin+trialdepth+wsp*28+trialidnum+'\n'  
+    
+    #write line to event file   
+    with open(ffname, 'a') as the_file:
+        if len(termline) != 73: #check that pick line is the correct length 
+            print('error: terminator line is incorrect length, not writing '+\
+            'to file') 
+            print(len(termline))
+            print(termline)
+        else:
+            the_file.write(termline)
     
     return
     
